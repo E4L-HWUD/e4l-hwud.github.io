e51a2482c48916154b002af2cef4d0fbff962c52
Aditya Shibu <as2397@hw.ac.uk>
2023-09-19 22:46:53 +0400

    Added contact form integration

---
+++ Google Sheet.js
@@ -0,0 +1,10 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzTfMVAoigdKoz5wxTD0iMam79WyqomNABxYF9uLOcmY4CFQdunBYLCKfJcT59Hq0rF/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thanks you! Your form is submitted successfully. We will contact you soon."))
+    .catch(error => console.error('Error!', error.message))
+})
